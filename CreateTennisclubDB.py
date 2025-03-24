7386bf13f4bd697ea12a58b8b0d72099e364c565
B00152361 Darryl Terrobias <b00152361@mytudublin.ie>
2025-03-24 17:02:17 +0000

    First Commit - Part 1 GUI

--- CreateTennisclubDB.py
+++ CreateTennisclubDB.py
@@ -1,4 +1,5 @@
 import sqlite3
+#CreateTennisclubDB
 
 # Connect to SQLite database (or create it if it doesn't exist)
 conn = sqlite3.connect("tennisclub.db")
--- MyFirstGUI.py
+++ MyFirstGUI.py
@@ -1,3 +1,4 @@
+#MyFirstGUI
 from tkinter import *
 
 class MyFirstGUI:
@@ -15,15 +16,33 @@ class MyFirstGUI:
         self.label2.pack()
         self.entry2 = Entry()
         self.entry2.pack()
+
+        self.label3 = Label(master, text="Date of Birth")
+        self.label3.pack()
+        self.entry3 = Entry()
+        self.entry3.pack()
+
+        self.label4 = Label(master, text="Enter Member Type")
+        self.label4.pack()
+        self.entry4 = Entry()
+        self.entry4.pack()
+
         
-        self.helloButton = Button(master, text="Hello", command=self.sayhello)
-        self.helloButton.pack()
+        self.InsertDB = Button(master, text="Insert Into Database", command=self.InsertDBFunction)
+        self.InsertDB.pack()
+
+        self.PrintMembers = Button(master, text="Print Members", command=self.PrintMembersFunction)
+        self.PrintMembers.pack()    
+
 
         self.closeButton = Button(master, text="Close", command=self.close)
         self.closeButton.pack()
 
-    def sayhello(self):    
-        print("Hello " + self.entry1.get())
+    def InsertDBFunction(self):    
+        print("Testing " + self.entry1.get())
+        
+    def PrintMembersFunction(self):    
+        print("Print Test " + self.entry1.get())
 
     def close(self):
         root.destroy()
